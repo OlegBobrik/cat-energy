2e8645b3ec53d839920751894ab91b6541823d28
Oleg V. Bobrik <olegvbobrik@gmail.com>
2018-09-28 15:09:24 +0500

    Добавлен browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+"use strict";
+var gulp        = require('gulp');
+var wait        = require('gulp-wait');
+var sass        = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
   return gulp.src('source/sass/style.scss')
+    .pipe(wait(200))
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('source/css'));
+    .pipe(gulp.dest('source/css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function () {
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: './source'
+    },
+    notify: false
+  });
   gulp.watch('source/sass/**/*.scss', ['sass']);
+  gulp.watch('source/*html').on('change', browserSync.reload);
 });
+
+gulp.task('watch', ['serve']);
