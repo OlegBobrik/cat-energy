e57138d66372cd19c4d82d380099f0f107e2cd50
Oleg V. Bobrik <olegvbobrik@gmail.com>
2018-11-01 15:18:24 +0500

    Слайдер, кнопка закрытия меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,47 @@
-var after = document.querySelector("slider__radio--after");
-var before = document.querySelector("slider__radio--before");
+var buttonMenu = document.querySelector(".page-header__button-menu");
+var menu = document.querySelector(".main-nav");
 
-console.log(after);
+var sliderPhoto = document.querySelector(".slider__photo");
+
+var after = document.querySelector(".slider__radio--after");
+var before = document.querySelector(".slider__radio--before");
+
+var scale = document.querySelector(".slider__scale");
+
+buttonMenu.addEventListener("click", function () {
+  menu.classList.toggle("main-nav--hiddden");
+  buttonMenu.classList.toggle("page-header__button-menu--close");
+})
+
+
+function check(radio) {
+  radio.checked = true;
+}
+
+function uncheck(radio) {
+  radio.checked = false;
+}
+
+before.addEventListener("click", function () {
+    scale.classList.add("slider__scale--right");
+    sliderPhoto.classList.add("slider__photo--after");
+});
+
+after.addEventListener("click", function () {
+  scale.classList.remove("slider__scale--right");
+  sliderPhoto.classList.remove("slider__photo--after");
+});
+
+scale.addEventListener("click", function() {
+  if (!scale.classList.contains("slider__scale--right")) {
+    scale.classList.add("slider__scale--right");
+    sliderPhoto.classList.add("slider__photo--after");
+    check(before);
+    uncheck(after);
+  } else {
+    scale.classList.remove("slider__scale--right");
+    sliderPhoto.classList.remove("slider__photo--after");
+    check(after);
+    uncheck(before);
+  }
+});
