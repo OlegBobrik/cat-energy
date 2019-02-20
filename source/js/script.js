a2463f8c5eb35366328462c01964a7a2a2c5976c
Bobrik V. Oleg <olegvbobrik@gmail.com>
2019-02-20 21:13:24 +0500

    Проверка наличия селекторов слайдера, добавление переменных для карты

--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,13 @@ var before = document.querySelector(".slider__radio--before");
 
 var scale = document.querySelector(".slider__scale");
 
+var widthMobile = 320;
+var zoomMap = 18;
+var iconOnMapWidth = 113;
+var iconOnMapHeight = 106;
+var positionIconX = -50;
+var positionIconY = -135;
+
 buttonMenu.addEventListener("click", function () {
   menu.classList.toggle("main-nav--close");
   buttonMenu.classList.toggle("page-header__button-menu--close");
@@ -21,44 +28,54 @@ function uncheck(radio) {
   radio.checked = false;
 }
 
-before.addEventListener("click", function () {
-    scale.classList.add("slider__scale--right");
-    sliderPhoto.classList.add("slider__photo--after");
-});
+if (before && after && sliderPhoto && scale) {
+  before.addEventListener("click", function () {
+      scale.classList.add("slider__scale--right");
+      sliderPhoto.classList.add("slider__photo--after");
+  });
 
-after.addEventListener("click", function () {
-  scale.classList.remove("slider__scale--right");
-  sliderPhoto.classList.remove("slider__photo--after");
-});
-
-scale.addEventListener("click", function() {
-  if (!scale.classList.contains("slider__scale--right")) {
-    scale.classList.add("slider__scale--right");
-    sliderPhoto.classList.add("slider__photo--after");
-    check(before);
-    uncheck(after);
-  } else {
+  after.addEventListener("click", function () {
     scale.classList.remove("slider__scale--right");
     sliderPhoto.classList.remove("slider__photo--after");
-    check(after);
-    uncheck(before);
-  }
-});
+  });
+
+  scale.addEventListener("click", function() {
+    if (!scale.classList.contains("slider__scale--right")) {
+      scale.classList.add("slider__scale--right");
+      sliderPhoto.classList.add("slider__photo--after");
+      check(before);
+      uncheck(after);
+    } else {
+      scale.classList.remove("slider__scale--right");
+      sliderPhoto.classList.remove("slider__photo--after");
+      check(after);
+      uncheck(before);
+    }
+  });
+}
 
 ymaps.ready(init);
+
+// if (document.querySelector(".address__map").offsetWidth == widthMobile) {
+//   iconOnMapWidth = 62;
+//   iconOnMapHeight = 53;
+//   zoomMap = 16;
+//   positionIconX = 20;
+//   positionIconY = -100;
+// }
 
 function init() {
     var myMap = new ymaps.Map("address__map", {
         center: [59.938631, 30.323055],
-        zoom: 18,
+        zoom: zoomMap,
         controls: ['geolocationControl', 'zoomControl', 'fullscreenControl']
     });
 
     var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
       iconLayout: 'default#image',
       iconImageHref: '/img/map-pin.png',
-      iconImageSize: [113, 106],
-      iconImageOffset: [-40, -135]
+      iconImageSize: [iconOnMapWidth, iconOnMapHeight],
+      iconImageOffset: [positionIconX, positionIconY]
   });
 
   myMap.geoObjects.add(myPlacemark);
